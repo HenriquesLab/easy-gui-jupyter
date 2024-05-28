8f950e84458b734cfe9f03ebfb21bb101fdf3db3
Ricardo Henriques <paxcalpt@gmail.com>
2024-05-28 11:59:22 +0100

    added lazydoc for documentation

---
+++ docs/README.md
@@ -0,0 +1,21 @@
+<!-- markdownlint-disable -->
+
+# API Overview
+
+## Modules
+
+- [`easy_gui_jupyter`](./easy_gui_jupyter.md#module-easy_gui_jupyter)
+
+## Classes
+
+- [`easy_gui_jupyter.EasyGUI`](./easy_gui_jupyter.md#class-easygui): A class to help simplify the creation of GUIs in Jupyter notebooks using ipywidgets.
+
+## Functions
+
+- [`easy_gui_jupyter.get_config`](./easy_gui_jupyter.md#function-get_config): Get the configuration dictionary without needing to initialize the GUI.
+- [`easy_gui_jupyter.save_config`](./easy_gui_jupyter.md#function-save_config): Save the configuration dictionary to file.
+
+
+---
+
+_This file was automatically generated via [lazydocs](https://github.com/ml-tooling/lazydocs)._
--- easy_gui_jupyter/easy_gui_jupyter.py
+++ easy_gui_jupyter/easy_gui_jupyter.py
@@ -1,23 +1,25 @@
+import yaml
+from ipyfilechooser import FileChooser
+import ipywidgets as widgets
+from IPython.display import display, clear_output
+from pathlib import Path
+
 """
 A module to help simplify the create of GUIs in Jupyter notebooks using ipywidgets.
 """
 
-import yaml
-from ipyfilechooser import FileChooser
-
-import ipywidgets as widgets
-from IPython.display import display, clear_output
-
-from pathlib import Path
-
-CONFIG_PATH = Path.home() / ".easy_gui"
+CONFIG_PATH = Path.home() / ".config" / "easy_gui"
 
 
-def get_config(title: str):
+def get_config(title: str) -> dict:
     """
     Get the configuration dictionary without needing to initialize the GUI.
 
-    :param title: title of the GUI
+    Args:
+        title (str): The title of the GUI.
+
+    Returns:
+        dict: The configuration dictionary.
     """
 
     config_file = CONFIG_PATH / f"{title}.yml"
@@ -38,10 +40,11 @@ def get_config(title: str):
 
 def save_config(title: str, cfg: dict):
     """
-    Save the configuration dictionary to a file.
+    Save the configuration dictionary to file.
 
-    :param title: title of the GUI
-    :param cfg: configuration dictionary
+    Args:
+        title (str): The title of the GUI.
+        cfg (dict): The configuration dictionary.
     """
     config_file = CONFIG_PATH / f"{title}.yml"
     config_file.parent.mkdir(exist_ok=True)
@@ -55,46 +58,16 @@ def save_config(title: str, cfg: dict):
 
 class EasyGUI:
     """
-    A class to simplify the creation of GUIs in Jupyter notebooks using ipywidgets.
-
-    Args:
-        title (str): The title of the GUI.
-        width (str): The width of the widget container (e.g., "50%").
-
-    Attributes:
-        _layout (ipywidgets.Layout): The layout for widgets.
-        _style (dict): Style configuration for widgets.
-        _widgets (dict): A dictionary to store widgets.
-        _nLabels (int): The number of labels added to the GUI.
-        _main_display (ipywidgets.VBox): The main output widget for the GUI.
-        _title (str): The title of the GUI.
-        _cfg (dict): Configuration dictionary to store widget values.
-        cfg (dict): Alias for the _cfg dictionary associated with the GUI.
-
-    Methods:
-        add_label: Add a label widget to the GUI.
-        add_button: Add a button widget to the GUI.
-        add_text: Add a text widget to the GUI.
-        add_int_slider: Add an integer slider widget to the GUI.
-        add_float_slider: Add a float slider widget to the GUI.
-        add_checkbox: Add a checkbox widget to the GUI.
-        add_int_text: Add an integer text widget to the GUI.
-        add_float_text: Add a float text widget to the GUI.
-        add_dropdown: Add a dropdown widget to the GUI.
-        add_file_upload: Add a file upload widget to the GUI.
-        save_settings: Save widget values to a configuration file.
-        show: Display the GUI with its widgets.
-        clear: Clear all widgets from the GUI.
-
-    Note:
-        This class simplifies the creation of GUIs in Jupyter notebooks using ipywidgets. It provides a variety of methods for adding different types of widgets to the GUI, and it allows for saving and loading widget values to maintain user settings across sessions.
+    A class to help simplify the creation of GUIs in Jupyter notebooks using ipywidgets.
     """
 
     def __init__(self, title="basic_gui", width="50%"):
         """
         Container for widgets.
-        :param title: title of the widget container
-        :param width: width of the widget container
+
+        Args:
+            title (str): The title of the widget container, used to store settings.
+            width (str): The width of the widget container.
         """
         self._layout = widgets.Layout(width=width)
         self._style = {"description_width": "initial"}
@@ -102,19 +75,36 @@ class EasyGUI:
         self._nLabels = 0
         self._main_display = widgets.VBox()
         self._title = title
-        self.cfg = get_config(title)
+        self._cfg = get_config(title)
 
     def __getitem__(self, tag: str) -> widgets.Widget:
+        """
+        Get a widget by tag.
+
+        Args:
+            tag (str): The tag of the widget.
+
+        Returns:
+            widgets.Widget: The widget.
+        """
         return self._widgets[tag]
 
     def __len__(self) -> int:
+        """
+        Get the number of widgets.
+
+        Returns:
+            int: The number of widgets.
+        """
         return len(self._widgets)
 
     def add_label(self, *args, **kwargs):
         """
         Add a label widget to the container.
-        :param args: args for the widget
-        :param kwargs: kwargs for the widget
+
+        Args:
+            args: Args for the widget.
+            kwargs: Kwargs for the widget.
         """
         self._nLabels += 1
         self._widgets[f"label_{self._nLabels}"] = widgets.Label(
@@ -124,9 +114,11 @@ class EasyGUI:
     def add_button(self, tag, *args, **kwargs):
         """
         Add a button widget to the container.
-        :param tag: tag to identify the widget
-        :param args: args for the widget
-        :param kwargs: kwargs for the widget
+
+        Args:
+            tag (str): The tag to identify the widget.
+            args: Args for the widget.
+            kwargs: Kwargs for the widget.
         """
         self._widgets[tag] = widgets.Button(
             *args, **kwargs, layout=self._layout, style=self._style
@@ -135,17 +127,22 @@ class EasyGUI:
     def add_text(self, tag, *args, remember_value=False, **kwargs):
         """
         Add a text widget to the container.
-        :param tag: tag to identify the widget
-        :param args: args for the widget
-        :param remember_value: remember the last value
-        :param kwargs: kwargs for the widget
+
+        Args:
+            tag (str): The tag to identify the widget.
+            args: Args for the widget.
+            remember_value (bool): Remember the last value.
+            kwargs: Kwargs for the widget.
 
         Example:
+            The following example demonstrates how to add a text widget to the GUI:
+
             >>> gui = EasyGUI()
-            >>> gui.add_text("text", value="Hello, world!")
+            >>> gui.add_text("text", "Enter some text:")
+            >>> gui.show()
         """
-        if remember_value and tag in self.cfg:
-            kwargs["value"] = str(self.cfg[tag])
+        if remember_value and tag in self._cfg:
+            kwargs["value"] = str(self._cfg[tag])
 
         self._widgets[tag] = widgets.Text(
             *args, **kwargs, layout=self._layout, style=self._style
@@ -154,13 +151,15 @@ class EasyGUI:
     def add_textarea(self, tag, *args, remember_value=False, **kwargs):
         """
         Add a textarea widget to the container.
-        :param tag: tag to identify the widget
-        :param args: args for the widget
-        :param remember_value: remember the last value
-        :param kwargs: kwargs for the widget
+
+        Args:
+            tag (str): The tag to identify the widget.
+            args: Args for the widget.
+            remember_value (bool): Remember the last value.
+            kwargs: Kwargs for the widget.
         """
-        if remember_value and tag in self.cfg:
-            kwargs["value"] = str(self.cfg[tag])
+        if remember_value and tag in self._cfg:
+            kwargs["value"] = str(self._cfg[tag])
         self._widgets[tag] = widgets.Textarea(
             *args, **kwargs, layout=self._layout, style=self._style
         )
@@ -172,17 +171,19 @@ class EasyGUI:
     def add_int_slider(self, tag, *args, remember_value=False, **kwargs):
         """
         Add a integer slider widget to the container.
-        :param tag: tag to identify the widget
-        :param args: args for the widget
-        :param remember_value: remember the last value
-        :param kwargs: kwargs for the widget
+
+        Args:
+            tag (str): The tag to identify the widget.
+            args: Args for the widget.
+            remember_value (bool): Remember the last value.
+            kwargs: Kwargs for the widget.
         """
         if (
             remember_value
-            and tag in self.cfg
-            and kwargs["min"] <= self.cfg[tag] <= kwargs["max"]
+            and tag in self._cfg
+            and kwargs["min"] <= self._cfg[tag] <= kwargs["max"]
         ):
-            kwargs["value"] = int(self.cfg[tag])
+            kwargs["value"] = int(self._cfg[tag])
         self._widgets[tag] = widgets.IntSlider(
             *args, **kwargs, layout=self._layout, style=self._style
         )
@@ -190,13 +191,15 @@ class EasyGUI:
     def add_float_slider(self, tag, *args, remember_value=False, **kwargs):
         """
         Add a float slider widget to the container.
-        :param tag: tag to identify the widget
-        :param args: args for the widget
-        :param remember_value: remember the last value
-        :param kwargs: kwargs for the widget
+
+        Args:
+            tag (str): The tag to identify the widget.
+            args: Args for the widget.
+            remember_value (bool): Remember the last value.
+            kwargs: Kwargs for the widget.
         """
-        if remember_value and tag in self.cfg:
-            kwargs["value"] = self.cfg[tag]
+        if remember_value and tag in self._cfg:
+            kwargs["value"] = self._cfg[tag]
         self._widgets[tag] = widgets.FloatSlider(
             *args, **kwargs, layout=self._layout, style=self._style
         )
@@ -204,13 +207,15 @@ class EasyGUI:
     def add_checkbox(self, tag, *args, remember_value=False, **kwargs):
         """
         Add a checkbox widget to the container.
-        :param tag: tag to identify the widget
-        :param args: args for the widget
-        :param remember_value: remember the last value
-        :param kwargs: kwargs for the widget
+
+        Args:
+            tag (str): The tag to identify the widget.
+            args: Args for the widget.
+            remember_value (bool): Remember the last value.
+            kwargs: Kwargs for the widget.
         """
-        if remember_value and tag in self.cfg:
-            kwargs["value"] = self.cfg[tag]
+        if remember_value and tag in self._cfg:
+            kwargs["value"] = self._cfg[tag]
         self._widgets[tag] = widgets.Checkbox(
             *args, **kwargs, layout=self._layout, style=self._style
         )
@@ -218,13 +223,15 @@ class EasyGUI:
     def add_int_text(self, tag, *args, remember_value=False, **kwargs):
         """
         Add a integer text widget to the container.
-        :param tag: tag to identify the widget
-        :param args: args for the widget
-        :param remember_value: remember the last value
-        :param kwargs: kwargs for the widget
+
+        Args:
+            tag (str): The tag to identify the widget.
+            args: Args for the widget.
+            remember_value (bool): Remember the last value.
+            kwargs: Kwargs for the widget.
         """
-        if remember_value and tag in self.cfg:
-            kwargs["value"] = self.cfg[tag]
+        if remember_value and tag in self._cfg:
+            kwargs["value"] = self._cfg[tag]
 
         self._widgets[tag] = widgets.IntText(
             *args, **kwargs, layout=self._layout, style=self._style
@@ -233,13 +240,15 @@ class EasyGUI:
     def add_float_text(self, tag, *args, remember_value=False, **kwargs):
         """
         Add a float text widget to the container.
-        :param tag: tag to identify the widget
-        :param args: args for the widget
-        :param remember_value: remember the last value
-        :param kwargs: kwargs for the widget
+
+        Args:
+            tag (str): The tag to identify the widget.
+            args: Args for the widget.
+            remember_value (bool): Remember the last value.
+            kwargs: Kwargs for the widget.
         """
-        if remember_value and tag in self.cfg:
-            kwargs["value"] = self.cfg[tag]
+        if remember_value and tag in self._cfg:
+            kwargs["value"] = self._cfg[tag]
         self._widgets[tag] = widgets.FloatText(
             *args, **kwargs, layout=self._layout, style=self._style
         )
@@ -247,17 +256,19 @@ class EasyGUI:
     def add_dropdown(self, tag, *args, remember_value=False, **kwargs):
         """
         Add a dropdown widget to the container.
-        :param tag: tag to identify the widget
-        :param args: args for the widget
-        :param remember_value: remember the last value
-        :param kwargs: kwargs for the widget
+
+        Args:
+            tag (str): The tag to identify the widget.
+            args: Args for the widget.
+            remember_value (bool): Remember the last value.
+            kwargs: Kwargs for the widget.
 
         Example:
             >>> gui = EasyGUI()
             >>> gui.add_dropdown("dropdown", options=["A", "B", "C"])
         """
-        if remember_value and tag in self.cfg and self.cfg[tag] in kwargs["options"]:
-            kwargs["value"] = self.cfg[tag]
+        if remember_value and tag in self._cfg and self._cfg[tag] in kwargs["options"]:
+            kwargs["value"] = self._cfg[tag]
         self._widgets[tag] = widgets.Dropdown(
             *args, **kwargs, layout=self._layout, style=self._style
         )
@@ -265,28 +276,34 @@ class EasyGUI:
     def add_file_upload(self, tag, *args, accept=None, multiple=False, **kwargs):
         """
         Add a file upload widget to the container.
-        :param tag: tag to identify the widget
-        :param args: args for the widget
-        :param accept: file types to accept
-        :param multiple: allow multiple files to be uploaded
-        :param kwargs: kwargs for the widget
+
+        Args:
+            tag (str): The tag to identify the widget.
+            args: Args for the widget.
+            accept: The file types to accept.
+            multiple (bool): Allow multiple files to be uploaded.
+            kwargs: Kwargs for the widget.
         """
         self._widgets[tag] = FileChooser()
         if accept is not None:
             self._widgets[tag].filter_pattern = accept
 
     def save_settings(self):
-        # remember widget values for next time and store them in a config file
+        """
+        Save the widget values to the configuration file.
+        """
         for tag in self._widgets:
             if tag.startswith("label_"):
                 pass
             elif hasattr(self._widgets[tag], "value"):
-                self.cfg[tag] = self._widgets[tag].value
+                self._cfg[tag] = self._widgets[tag].value
 
-        save_config(self._title, self.cfg)
+        save_config(self._title, self._cfg)
 
     def restore_default_settings(self):
-        # restore default settings
+        """
+        Restore the default settings and clear the widgets.
+        """
         save_config(self._title, {})
         self.clear()
         self.show()
@@ -295,7 +312,6 @@ class EasyGUI:
         """
         Show the widgets in the container.
         """
-        # display the widgets
         self._main_display.children = tuple(self._widgets.values())
         clear_output()
         display(self._main_display)
